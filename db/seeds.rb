dcf97b47895d2230cda1c52cd24786fa2ecab280
Scott Fox <scottfox87@gmail.com>
2018-03-12 15:04:45 -0500

    create airports table and add seeds)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Airport.delete_all
+Airport.create!([
+                    {name: "PDX"},
+                    {name: "YUL"},
+                    {name: "MDW"},
+                    {name: "SEA"}
+                ])
