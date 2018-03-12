25c2dbfad9febcd326aa6c94f8660ea130468ad7
Scott Fox <scottfox87@gmail.com>
2018-03-12 16:46:59 -0500

    fix associations for airport arrivals and departures returning flight objects

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,4 @@
 class Airport < ApplicationRecord
-  has_many :departures, foreign_key: "start_city", class_name: "Flight"
-  has_many :arrivals, foreign_key: "end_city", class_name: "Flight"
+  has_many :departures, foreign_key: "start_city_id", class_name: "Flight"
+  has_many :arrivals, foreign_key: "end_city_id", class_name: "Flight"
 end
