f85b9f5e8be9ecb2bc5e3a09536de143e9d11e07
Scott Fox <scottfox87@gmail.com>
2018-03-13 16:05:18 -0500

    add date and passenger number dropdowns, and corresponding controller logic

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,13 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all.map { |a| [a.airport_code, a.id] }
+    @dates_ordered = Flight.all.order(:start_time)
+    @dates = @dates_ordered.all.map { |d| d.start_time.strftime("%m/%d/%Y")}
+    @dates.uniq!
+    @passengers = [1,2,3,4]
     @from_chosen = params[:from]
     @to_chosen = params[:to]
+    @date_chosen = params[:date]
+    @passengers_chosen= params[:passengers]
   end
 end
