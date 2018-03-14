afe15dcca0621cbefa0f4d16af2d6f736150eaa2
Scott Fox <scottfox87@gmail.com>
2018-03-14 13:46:08 -0500

    create connections so that search returns list of flights leaving on specified date

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,13 +1,22 @@
 class FlightsController < ApplicationController
   def index
+
+    #select_tag needs an array of airport objects with display and id
     @airports = Airport.all.map { |a| [a.airport_code, a.id] }
+    #order flights by their start time
     @dates_ordered = Flight.all.order(:start_time)
+    #then make an array of dates for select_tag helper
     @dates = @dates_ordered.all.map { |d| d.start_time.strftime("%m/%d/%Y")}
+    #get rid of duplicate dates
     @dates.uniq!
+    #make array of passenger number for select_tag
     @passengers = [1,2,3,4]
+    #variables to hold info passed in from params, the first argument of a select_tag
     @from_chosen = params[:from]
     @to_chosen = params[:to]
     @date_chosen = params[:date]
     @passengers_chosen= params[:passengers]
+
+    @flights = Flight.search(params).order(:start_time)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,20 @@ class Flight < ApplicationRecord
   belongs_to :from_airport, foreign_key: "start_city_id", class_name: "Airport"
   belongs_to :to_airport, foreign_key: "end_city_id", class_name: "Airport"
 
+  def self.search(params)
+    if params[:search_clicked]
+      date_formatted = format_date(params[:date])
+      Flight.where("start_city_id = ? AND end_city_id = ? AND start_time LIKE ?", params[:from], params[:to], "%#{date_formatted}%")
+    else
+      Flight.none
+    end
+
+  end
+
+  def self.format_date(date)
+    year = date[6..9] #extract year
+    month = date[0..1] #extract month
+    day = date[3..4] #extract day
+    return "#{year}-#{month}-#{day}"
+  end
 end
