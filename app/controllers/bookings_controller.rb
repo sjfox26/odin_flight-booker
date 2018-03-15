5bf6f14f492b3402fa7385510f15f13670090108
Scott Fox <scottfox87@gmail.com>
2018-03-15 16:47:21 -0500

    add booking create and show logic and flight confirmation page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,25 @@ class BookingsController < ApplicationController
     params[:passenger_count].to_i.times { @booking.passengers.build }
   end
 
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      flash[:success] = "Your flight has been booked!  Enjoy your travels!"
+      redirect_to @booking
+    else
+      flash[:error] = "You must enter all information for all passengers."
+      render :new
+    end
+  end
+
   def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def booking_params
+    params.require(:booking).permit(:flight_id, :passengers_attributes => [:id, :name, :email])
   end
 end
